42a07eef11d4a6b1c10a20903c0d4aa0ebc78ac5
dhaval <developer.dhavalp@gmail.com>
2018-09-18 11:50:21 +0530

    jwt token authentication changes

---
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody]UserModel model)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(model);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private string GenerateJWTToken(UserModel model)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, model.FullName),
+                new Claim(JwtRegisteredClaimNames.Sub, model.Email),
+                new Claim("DateOfJoining", model.DateOfJoin.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+       
+        private UserModel AuthenticateUser(UserModel model)
+        {
+            // UserModel user = null;
+            if (model.Email.ToLower() == "test@gmail.com" && model.Password.ToLower() == "123")
+            {
+                model = new UserModel { Email = "testuser@gmail.com", FullName = "abc xyz", DateOfJoin = DateTime.Now.AddYears(-6).ToString() };
+            }
+            return model;
+        }
+
+        [AllowAnonymous]
+        public class UserModel
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string FullName { get; set; }
+            public string DateOfJoin { get; set; }
+        }
+    }
+}
---
+++ JWTAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendtimeWithCompany = 0;
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoining"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoining").Value); ;
+                spendtimeWithCompany = DateTime.Now.Year - date.Year;
+            }
+
+            if (spendtimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
